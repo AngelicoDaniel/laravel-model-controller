1e8761d1890b91bb9b86a17e3407bfa4547819aa
Daniel <angelicodaniel93@gmail.com>
2023-01-16 16:42:44 +0100

    last commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,22 @@
 @section('page-title', 'movies')
 
 @section('main-content')
-    <h1>Movies:</h1>
+    <h1 class="text-center">Movies</h1>
     {{-- insert movies from db --}}
-    @foreach ($movies as $elem)
-        <h2>{{$elem['title']}}</h2>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $elem)       
+                    <div class="col-3 g-2">
+                        <div class="card bg-secondary" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$elem['title']}}</h5>
+                                <p class="card-text">Nationality: {{$elem['nationality']}}</p>
+                                <p class="card-text">Date: {{$elem['date']}}</p>
+                                <p class="card-text">Date: {{$elem['vote']}}</p>
+                            </div>
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+    </div>   
 @endsection
