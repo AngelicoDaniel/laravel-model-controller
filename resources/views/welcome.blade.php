860b599c945e3b4ff509bd09bba8686e996d76d6
Daniel <angelicodaniel93@gmail.com>
2023-01-16 16:16:47 +0100

    last commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,7 @@
 @section('main-content')
     <h1>Movies:</h1>
     {{-- insert movies from db --}}
+    @foreach ($movies as $elem)
+        <h2>{{$elem['title']}}</h2>
+    @endforeach
 @endsection
